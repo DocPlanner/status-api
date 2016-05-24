7f5a055e4a9d4592fb00632c530ee7b8c6ebd79e
gellu <grkrysiak@gmail.com>
2016-05-24 13:58:51 +0200

    refactor SDK for cachetHQ part 2 (fix)

--- api.php
+++ api.php
@@ -31,7 +31,7 @@ $app->get('/webhook/pingdom', function() use ($app) {
 	$pingdomAlert->setPayload($app->request()->get('message'));
 
 	(new StatusPage())->update($pingdomAlert);
-	(new Cachet())->updateComponent($pingdomAlert);
+	(new Cachet())->trigger($pingdomAlert);
 
 });
 
